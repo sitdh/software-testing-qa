80f0f64335d16628d1a01d2401e6fd1613b1f5b5
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-09-28 18:47:08 +0700

    Add range checking

--- homework/w5-graph-draw/src/asset/js/bundle.js
+++ homework/w5-graph-draw/src/asset/js/bundle.js
@@ -10,14 +10,20 @@ $(window).ready(function(){
         var b = parseInt( $('#b_number').val() );
         var c = parseInt( $('#c_number').val() );
 
-        if ( a <= b + c && b <= a + c && c <= a + b ) {
+        if ( ( a < 0 || a > 200 ) && ( b < 0 || b > 200 ) && ( c < 0 || c > 200 ) ) {
+            triangleType = 'Some input value is out of range';
+        } else {
 
-            if ( a == b && a == c ) {
-                triangleType = 'Equilateral';
-            } else if ( a != b && b != c && c != a ) {
-                triangleType = 'Scalene';
-            } else {
-                triangleType = 'Isoscalene';
+            if ( a <= b + c && b <= a + c && c <= a + b ) {
+
+                if ( a == b && a == c ) {
+                    triangleType = 'Equilateral';
+                } else if ( a != b && b != c && c != a ) {
+                    triangleType = 'Scalene';
+                } else {
+                    triangleType = 'Isoscalene';
+                }
+
             }
 
         }
