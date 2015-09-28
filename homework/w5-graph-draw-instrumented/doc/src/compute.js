11bef793161693207245203ddd6b4ec54636a42e
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-09-28 21:00:49 +0700

    Add instrumented code

---
+++ homework/w5-graph-draw-instrumented/doc/src/compute.js
@@ -0,0 +1,18 @@
+var triangleType = 'Not a triangle';
+var a = parseInt( $('#a_number').val() );
+var b = parseInt( $('#b_number').val() );
+var c = parseInt( $('#c_number').val() );
+if ((a<0 || a>200) || (b<0 || b>200 ) || (c<0 || c>200)) {
+    triangleType = 'Some input value is out of range';
+} else {
+    if ( a <= b + c && b <= a + c && c <= a + b ) {
+        if ( a == b && a == c ) {
+            triangleType = 'Equilateral';
+        } else if ( a != b && b != c && c != a ) {
+            triangleType = 'Scalene';
+        } else {
+            triangleType = 'Isoscalene';
+        }
+    }
+}
+$('#log-msg').append('Triangle type: ' + triangleType + "\n");
