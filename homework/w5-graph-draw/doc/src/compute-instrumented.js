cbb03f7673c850a12972c03bbb05304c99cf465d
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-10-03 23:23:25 +0700

    Add parenthesises

--- homework/w5-graph-draw/doc/src/compute-instrumented.js
+++ homework/w5-graph-draw/doc/src/compute-instrumented.js
@@ -9,14 +9,14 @@ $('#log-msg').append("Triangle's sides received<br>");
 if ((a<0 || a>200) || (b<0 || b>200) || (c<0 || c>200 ) ) {
     $('#log-msg').append('Some input value is out of range<br>');
     triangleType = 'Some input value is out of range';
-} else if ( a < b + c && b < a + c && c < a + b ) {
+} else if ( (a<b+c) && (b<a+c) && (c<a+b) ) {
     $('#log-msg').append('Sides should be formed in triangle<br>');
 
     if ( a == b && a == c ) {
         $('#log-msg').append('Sides are equal<br>');
         triangleType = 'Equilateral';
 
-    } else if ( a*a == b*b + c*c || b*b == c*c + a*a || c*c == a*a + b*b ) {
+    } else if ( (a*a==b*b+c*c) || (b*b==c*c+a*a) || (c*c==a*a+b*b) ) {
         $('#log-msg').append("Pythagorus check<br>");
         triangleType = 'Right Triangle';
 
--- homework/w5-graph-draw/src/asset/js/bundle.js
+++ homework/w5-graph-draw/src/asset/js/bundle.js
@@ -12,11 +12,11 @@ $(window).ready(function(){
 
         if ((a<0 || a>200) || (b<0 || b>200) || (c<0 || c>200 ) ) {
             triangleType = 'Some input value is out of range';
-        } else if ( a < b + c && b < a + c && c < a + b ) {
+        } else if ( (a < b + c) && (b < a + c) && (c < a + b) ) {
 
             if ( a == b && a == c ) {
                 triangleType = 'Equilateral';
-            } else if ( a*a == b*b + c*c || b*b == c*c + a*a || c*c == a*a + b*b ) {
+            } else if ( (a*a == b*b + c*c) || (b*b == c*c + a*a) || (c*c == a*a + b*b) ) {
                 triangleType = 'Right Triangle';
             } else if ( a != b && b != c && c != a ) {
                 triangleType = 'Scalene';
